4f6a94bc1e0177948f92563276b52e29c6e3f3aa
manishawsfreelancer <manishawsfreelancer@gmail.com>
2021-10-06 23:10:47 +0530

    .

--- pragnika/create_loggroups.py
+++ pragnika/create_loggroups.py
@@ -1,20 +1,25 @@
 from logging import log
 import boto3
+import sys
 
 #This python script will read the loggroups names from the file and create those in 
 # the aws cloud.
 
 #Read the loggroups from text file and create the CW loggroup.
 
+env = sys.argv[1]
+
 client = boto3.client('logs')
 
 try:
     with open('test_loggroups.txt',"r") as myfile:
         for i in myfile:
-            loggroup_name = i.rstrip('\n')
-            client.create_log_group(
-                logGroupName=loggroup_name
-            )
-            print("loggroup_name created successfully")
+            appshort_name = i.rstrip('\n')
+            loggroup_name = appshort_name + env + 'edl' + 'emr'
+            if loggroup_name in client.describe_log_groups(logGroupNamePrefix=loggroup_name)['logGroups']:
+                print(f"{loggroup_name} already exisisting")
+            else:
+                client.create_log_group(logGroupName=loggroup_name)
+                print("loggroup_name created successfully")
 except Exception as err:
     print(err)
