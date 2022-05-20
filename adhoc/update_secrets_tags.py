3bf353f5070cba7696908c5c373ab6ffc1d6d13b
manishawsfreelancer <60460249+manishawsfreelancer@users.noreply.github.com>
2022-05-20 10:22:07 +0530

    Update update_secrets_tags.py

--- adhoc/update_secrets_tags.py
+++ adhoc/update_secrets_tags.py
@@ -1,1 +1,13 @@
 import boto3
+import json
+secrets_client = boto3.client('secretsmanager')
+secret_id = ''
+response = client.tag_resource(
+    SecretId=secret_id,
+    Tags=[
+        {
+            'Key': 'Test_Update_Tags',
+            'Value': 'Testing_Successfull'
+        },
+    ]
+)
